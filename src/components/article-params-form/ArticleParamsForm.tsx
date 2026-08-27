f7edc24e63fe252f497c716e0bac262188778895
Earl of Tureyev <lider-1@mail.ru>
2026-08-27 16:22:11 +0500

    feat: add click outside close

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { ArrowButton } from 'src/ui/arrow-button';
 import styles from './ArticleParamsForm.module.scss';
 
@@ -6,8 +6,28 @@ export const ArticleParamsForm = () => {
 	const [isOpen, setIsOpen] = useState(false);
 	const toggleOpen = () => setIsOpen(!isOpen);
 
+	const sidebarRef = useRef<HTMLDivElement>(null);
+
+	useEffect(() => {
+		const handleClickOutside = (e: MouseEvent) => {
+			if (
+				sidebarRef.current &&
+				!sidebarRef.current.contains(e.target as Node)
+			) {
+				setIsOpen(false);
+			}
+		};
+
+		if (isOpen) {
+			document.addEventListener('mousedown', handleClickOutside);
+		}
+		return () => {
+			document.removeEventListener('mousedown', handleClickOutside);
+		};
+	}, [isOpen]);
+
 	return (
-		<div>
+		<div ref={sidebarRef}>
 			<ArrowButton isOpen={isOpen} onClick={toggleOpen} />
 			{isOpen && <aside className={styles.container}>Панель открыта</aside>}
 		</div>
